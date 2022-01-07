bb35e04f2a80e04fb91cbe7fbdaa872785491899
Bigizi <bigizinduwayo@gmail.com>
2022-01-07 06:38:55 -0800

    index changed to php

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+		
+		<title>G&B</title>
+		
+		<!-- Loading third party fonts -->
+		<link href="fonts/font-awesome.min.css" rel="stylesheet" type="text/css">
+		<link href="fonts/novecento-font/novecento-font.css" rel="stylesheet" >
+
+		<!-- Loading main css file -->
+		<link href="style.css" rel="stylesheet" >
+		
+		<!--[if lt IE 9]>
+		<script src="js/ie-support/html5.js"></script>
+		<script src="js/ie-support/respond.js"></script>
+		<![endif]-->
+
+	</head>
+
+
+	<body>
+		
+		<div id="site-content">
+			
+			<header class="site-header">
+				<div class="container">
+					<a href="index.html" id="branding">
+						<img src="images/logo.png" alt="Company Name" class="logo">
+						<div class="branding-copy">
+							<h1 class="site-title"> G&B VIRTUS ATTORNEYS</h1>
+							<small class="site-description">Call us on: <i class="fa fa-phone" aria-hidden="true">+250 788 550 811</i></small><br>
+							<small class="site-description">Call us on: <i class="fa fa-phone" aria-hidden="true">+250 788 357 971</i></small><br>
+							<small class="site-description">Location: <i class="fa fa-map-marker" aria-hidden="true">Kigali, Kicukiro, Niboyi</i></small>
+						</div>
+						
+					</a>
+
+					<nav class="main-navigation">
+						<button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
+						<ul class="menu">
+							<li class="menu-item current-menu-item"><a href="index.html">Home</a></li>
+							<li class="menu-item"><a href="about.html">About Us</a></li>
+							<li class="menu-item"><a href="service.html">Services</a></li>
+							<li class="menu-item"><a href="contact.html">Contact</a></li>
+						</ul>
+					</nav>
+					<nav class="mobile-navigation"></nav>
+				</div>
+			</header> <!-- .site-header -->
+
+			<main class="main-content">
+				<div class="fullwidth-block" data-bg-color="#111113">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-8">
+								<h2>Welcome to G&B VIRTUS ATTORNEYS</h2>
+								<p> We are a legal firm based in Kigali Rwanda, what we offer is detailed in ours services.</p>
+							</div>
+							<div class="col-md-8">
+								<h1> G&B VIRTUS ATTORNEYS</h1>
+								<p>“Devoted to serve you with competence”</p>
+							</div>
+						</div> <!-- .row -->
+					</div> <!-- .container -->
+				</div> <!-- .fullwidth-block -->
+
+				<div class="fullwidth-block">
+					<div class="container">
+						<div class="row feature-list-section">
+							<div class="col-md-4">
+								<div class="feature">
+									<header>
+										<img src="images/icon-1.png" class="feature-icon">
+										<div class="feature-title-copy">
+											<h2 class="feature-title">Legal information and advice</h2>
+										</div>
+									</header>
+									<p>G&B Virtus Attorneys give the legal information as well as the written or oral counsel about a legal matter that would affect your rights and responsibilities.</p>
+									<a href="#" class="more-link">Read More</a>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="feature">
+									<header>
+										<img src="images/icon-2.png" class="feature-icon">
+										<div class="feature-title-copy">
+											<h2 class="feature-title">Assisting and representing </h2>
+										</div>
+									</header>
+									<p>G&B Virtus Attorneys assist and/or represent the clients before administrative entities, criminal investigation and prosecution institutions as well as courts and other decision-making organs. </p>
+									<a href="#" class="more-link">Read More</a>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="feature">
+									<header>
+										<img src="images/icon-3.png" class="feature-icon">
+										<div class="feature-title-copy">
+											<h2 class="feature-title"><br>Reviewing<br>documents<br></h2>
+										</div>
+									</header>
+									<p>G&B Virtus Attorneys give the consultation to review documents such as agreements, leases, wills, corporation documents, do-it-yourself court forms, etc.</p>
+									<a href="#" class="more-link">Read More</a>
+								</div>
+							</div>
+						</div>
+						<div class="row feature-list-section"></div>
+						<div class="col-md-4">
+							<div class="feature">
+								<header>
+									<img src="images/icon-3.png" class="feature-icon">
+									<div class="feature-title-copy">
+										<h2 class="feature-title">Mediation and arbitration</h2>
+									</div>
+								</header>
+								<p>G&B Virtus Attorneys give the consultation to review documents such as agreements, leases, wills, corporation documents, do-it-yourself court forms, etc.</p>
+								<a href="#" class="more-link">Read More</a>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="feature">
+								<header>
+									<img src="images/icon-3.png" class="feature-icon">
+									<div class="feature-title-copy">
+										<h2 class="feature-title"><br><br></r>Consultancies</h2>
+									</div>
+								</header>
+								<p>G&B Virtus Attorneys give expert advice in the areas of access to justice and human rights.</p>
+								<a href="#" class="more-link">Read More</a>
+							</div>
+						</div>
+					</div>
+					</div>
+				</div>
+
+			</main> <!-- .main-content -->
+
+			<footer class="site-footer">
+				<div class="container">
+					<div class="subscribe-form">
+						<form action="#">
+							<label for="#">
+								<span>Do you want to get news?</span>
+								<span>Join our news letter</span>
+							</label>
+							<div class="control">
+								<input type="text" placeholder="Enter your email to subscribe...">
+								<button type="submit"><img src="images/icon-envelope.png" alt=""></button>
+														</form>
+							</div>
+					</div>
+					<div class="social-links">
+						<a href="#"><i class="fa fa-facebook"></i></a>
+						<a href="#"><i class="fa fa-twitter"></i></a>
+						<a href="#"><i class="fa fa-google-plus"></i></a>
+						<a href="#"><i class="fa fa-pinterest"></i></a>
+					</div>
+					<div class="copy">
+						<p>Copyright 2022 G&B Virtus Attorneys . Designed by Bigizi Crispin. All rights reserved.</p>
+					</div>
+				</div>
+			</footer> <!-- .site-footer -->
+
+		</div> <!-- #site-content -->
+
+		
+
+		<script src="js/jquery-1.11.1.min.js"></script>
+		<script src="js/plugins.js"></script>
+		<script src="js/app.js"></script>
+		
+	</body>
+
+</html>
